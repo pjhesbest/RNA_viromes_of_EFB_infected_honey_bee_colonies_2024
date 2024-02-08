48cd11f9de6c7e02848ee73f9d84035466026d51
pjhesbest <62026938+pjhesbest@users.noreply.github.com>
2024-02-08 09:01:28 -0600

    Update iq-tree_phylogeny.sh

--- iq-tree_phylogeny.sh
+++ iq-tree_phylogeny.sh
@@ -11,19 +11,21 @@ Help()
 {
 echo -e "This script is to perform the following:\n\t\t\t(1) Multiple sequence alignment with MAFFT\n\t\t\t(2) Trim alignment using TrimAL\n\t\t\t(3) Phylogenetic reconstruction with IQ-TREE\n"
 echo -e "-i, --input\t/full/path/to/file.fasta for MSA and phylogeny \e[31m[Required]\e[0m"
-echo -e "-b, --bootstraps\tNumber of boottraps for tree \e[31m[Default: -b 200 ]\e[0m"
+echo -e "-b, --bootstraps\tNumber of boottraps for tree \e[31m[Default: -b 200]\e[0m"
 echo -e "-R, --Retree\tRerun only the tree using existing alignment \e[31m[Optional]\e[0m"
+echo -e "-t, --threads\tNumber of threads \e[31m[Default: -t 4]\e[0m"
 }
 
 ##################################################################################################
 ##################################################################################################
 
-while getopts i:b:R:h option
+while getopts i:b:R:t:h option
 do 
     case "${option}" in
         i)input=${OPTARG};;
-		b)bootstraps=${OPTARG};;
-		R)retree=true;;
+	b)bootstraps=${OPTARG};;
+	R)retree=true;;
+  	t)threads=${OPTARG};;
     h)Help; exit;;
     esac
 
@@ -35,6 +37,7 @@ done
 	# check flags are used or set defaults
 if [[ -z "${input}" ]]; then echo "-i, --input REQUIRED"; Help; exit; fi
 if [[ -z "${bootstraps}" ]]; then bootstraps="200"; fi
+if [[ -z "${threads}" ]]; then threads="4"; fi
 
 	# create prefix variable for naming outputs
 prefix=$(basename ${input} .gz | sed 's/\.fasta//g; s/\.fa//g; s/\.fq//g; s/\.fastq//g; s/\.fna//g')
